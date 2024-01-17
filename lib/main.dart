2d0e9d6bf29e068431615873a51246b52bb1ce5e
Shukhrat0531 <shuhrat0531@gmail.com>
2024-01-17 23:22:26 +0600

    Intagrate firbase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:chat_flutter/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: 'AIzaSyCFVEqBI50te4i2pjoora7kc-IMZfxWJVg',
+          appId: '1:892427771067:android:2b07c768b0814ae5c55d9e',
+          messagingSenderId: '892427771067',
+          projectId:'chat-app-20956')
+          );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,8 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        scaffoldBackgroundColor:
-            Colors.white,
+        scaffoldBackgroundColor: Colors.white,
       ),
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
--- lib/screens/chat_details_screen.dart
+++ lib/screens/chat_details_screen.dart
@@ -14,56 +14,56 @@ class _ChatDetailScreenState extends State<ChatDetailScreen> {
       appBar: AppBar(
         toolbarHeight: 70,
         backgroundColor: Colors.white,
-        leading:Padding(
-          padding: const EdgeInsets.only(top: 10),
+        leading: Padding(
+          padding: const EdgeInsets.only(top: 5, left: 10),
           child: Row(
             children: [
               InkWell(
-                onTap: () {
-                  
-                },
-                child: Image.asset('assets/images/btn.png')),
-               const Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 25,
-                  ),
-                  SizedBox(
-                      width:
-                          8), // Добавьте промежуток между элементами, если нужно
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Иванов Тван", style: TextStyle(color: Colors.black)),
-                      Text('в сети', style: TextStyle(color: Color.fromARGB(255, 108, 108, 108))),
-                    ],
-                  ),
-                ],
+                  onTap: () {}, child: Image.asset('assets/images/btn.png')),
+              const Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 25,
+                    ),
+                    SizedBox(width: 8),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Иванов Тван",
+                            style: TextStyle(color: Colors.black)),
+                        Text('в сети',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 108, 108, 108))),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            ],),
-        ), 
+            ],
+          ),
+        ),
         elevation: 0.5,
         leadingWidth: 200,
       ),
       body: Container(),
       bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(left: 15,bottom: 10),
+        padding: const EdgeInsets.only(left: 15, bottom: 10),
         child: Row(
           children: [
             Container(
-              height: 42,
-              width: 42,
-              decoration: BoxDecoration(
-                color: const Color(0xFFEDF2F6),
-                borderRadius: BorderRadius.circular(12)
-              ),
-              child: Image.asset('assets/images/Attach.png')),
-              const SizedBox(width: 10,),
-               Container(
+                height: 42,
+                width: 42,
+                decoration: BoxDecoration(
+                    color: const Color(0xFFEDF2F6),
+                    borderRadius: BorderRadius.circular(12)),
+                child: Image.asset('assets/images/Attach.png')),
+            const SizedBox(
+              width: 10,
+            ),
+            Container(
               decoration: BoxDecoration(
                 color: const Color(0xFFEDF2F6),
                 borderRadius: BorderRadius.circular(12),
@@ -80,7 +80,9 @@ class _ChatDetailScreenState extends State<ChatDetailScreen> {
                     )),
               ),
             ),
-            const SizedBox(width: 10,),
+            const SizedBox(
+              width: 10,
+            ),
             Container(
                 height: 42,
                 width: 42,
